70c29855837048c822adf01d47960e0acf80fb41
cjsmi <ClarenceSmith90@gmail.com>
2024-06-26 21:53:23 -0400

    new controls

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import * as BABYLON from "@babylonjs/core/Legacy/legacy";
-console.log("Let's fucking go");
+// import * as BABYLON from "@babylonjs/core/Legacy/legacy";
+
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
@@ -20,14 +20,13 @@ const engine = new Engine(canvas);
 var scene = new Scene(engine);
 
 // This creates and positions a free camera (non-mesh)
-// var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-var camera = new BABYLON.ArcRotateCamera("camera2", 2, 1, 13, new BABYLON.Vector3(0, 0, 0), scene);
+var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 
 // This targets the camera to scene origin
 // camera.setTarget(Vector3.Zero());
 
 // This attaches the camera to the canvas
-camera.attachControl(canvas, true);
+// camera.attachControl(canvas, true);
 
 // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
 // var light = new HemisphericLight("light1", new Vector3(1, 0, 0), scene);
@@ -48,20 +47,43 @@ sphere.position.y = 2;
 sphere.material = material;
 
 // Our built-in 'ground' shape.
-var ground = CreateGround("ground1", { width: 6, height: 6, subdivisions: 2 }, scene);
+var ground = CreateGround("ground1", { width: 25, height: 25, subdivisions: 4 }, scene);
 
 // Affect a material
 ground.material = material;
-let x = 0;
-let y = 0;
-let z = 0;
+
+var camera2 = new ArcRotateCamera("camera2", 1, Math.PI*3/7, 20, sphere, scene, true);
+camera2.attachControl(canvas,);
+camera2.inputs.attached.keyboard.detachControl();
+// scene.activeCamera = camera2;
+
+
+
+canvas.addEventListener('keydown', (e) => {
+  const key = e.key;
+  let vertical = sphere.position.subtractInPlace(camera2.position).normalize();
+  vertical.y = 0;
+  let horizontal = new Vector3(vertical.z, 0, -vertical.x);
+  switch(key){
+    case "ArrowLeft":
+      sphere.position.addInPlace(horizontal);
+      break;
+    case "ArrowRight":
+      sphere.position.subtractInPlace(horizontal);
+      break;
+    case "ArrowUp":
+      sphere.position.subtractInPlace(vertical);
+      break;
+      case "ArrowDown":
+      sphere.position.addInPlace(vertical);
+      break;
+    default:
+      return;
+  }
+})
+
+
 // Render every frame
 engine.runRenderLoop(() => {
-  y += 0.03;
-  x += 0.1;
-  z += 0.1;
-  sphere.position.x = 2 * Math.cos(x);
-  sphere.position.y = 3 + 2 * Math.cos(y);
-  sphere.position.z = 2 * Math.sin(z);
   scene.render();
 });
